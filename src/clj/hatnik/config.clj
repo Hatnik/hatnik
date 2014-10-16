60e7d4ee69770ef07b35df1a8c6eb2212afc790f
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-10-16 07:53:11 +0100

    Locate cider bug.

--- src/clj/hatnik/config.clj
+++
@@ -1,7 +0,0 @@
-(ns hatnik.config)
-
-(defn get-config []
-  (merge
-   (read-string (slurp "config.default.clj"))
-   (try (read-string (slurp "config.clj"))
-        (catch Exception e {}))))
---
+++ test/cider_test.clj
@@ -0,0 +1,8 @@
+(ns hatnik.db.memory-storage-test
+  (:require [clojure.test :refer :all]
+            [taoensso.timbre :as timbre]))
+
+(deftest hello-test
+  (timbre/info "hello")
+  (is true))
+
