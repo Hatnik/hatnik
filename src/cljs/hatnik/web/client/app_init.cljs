c9d3a50e34c47e4f591fc0354288cdb722dc7520
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-11-08 23:34:04 +0000

    Enable logging using println.

--- src/cljs/hatnik/web/client/app_init.cljs
+++ src/cljs/hatnik/web/client/app_init.cljs
@@ -4,6 +4,8 @@
             [hatnik.web.client.components :as widget]
             [hatnik.web.client.app-state :as state]))
 
+(enable-console-print!)
+
 ;(om/root widget/project-list state/app-state
 ;         {:target (. js/document (getElementById "iProjectList"))}) 
 
