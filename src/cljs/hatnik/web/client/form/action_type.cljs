0e2eabf511f8e47c9259c3bd52ed9e1f9472dfeb
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-11-08 23:32:18 +0000

    Make action-type component to update state directly.

--- src/cljs/hatnik/web/client/form/action_type.cljs
+++ src/cljs/hatnik/web/client/form/action_type.cljs
@@ -5,24 +5,30 @@
             [hatnik.schema :as schm]
             [schema.core :as s]))
 
-(defn option-element-map [val pred]
-  (if pred
-    #js {:className "form-control" :selected "selected" :value val}
-    #js {:className "form-control" :value val}))
+(def types
+  [{:value "noop"
+    :text "Noop"}
+   {:value "email"
+    :text "Email"}
+   {:value "github-issue"
+    :text "GitHub Issue"}])
 
 (defn action-type-component [data owner]
   (reify
     om/IRender
     (render [this]
-      (let [callback (:handler data)]
-        (dom/div #js {:className "form-group action-type-component"}
-                 (dom/label #js {:htmlFor "action-type"
-                                 :className "control-label"} "Action type")
-                 (dom/select #js {:className "form-control"
-                                  :id "action-type"
-                                  :defaultValue (name (:type data))
-                                  :onChange #(callback (keyword (.. % -target -value)))}
-                             (dom/option (option-element-map "email" (= :email (:type data)))  "Email")
-                             (dom/option (option-element-map "noop" (= :noop (:type data))) "Noop")
-                             (dom/option (option-element-map "github-issue" (= :github-issue (:type data))) "GitHub issue")))))))
+      (dom/div #js {:className "form-group action-type-component"}
+               (dom/label #js {:htmlFor "action-type"
+                               :className "control-label"} "Action type")
+               (apply dom/select #js {:className "form-control"
+                                      :id "action-type"
+                                      :defaultValue (:type data)
+                                      :onChange #(om/update! data :type (.. % -target -value))}
+                      (for [{:keys [value text]} types]
+                        (dom/option #js {:className "form-control"
+                                         :value value
+                                         :selected (if (= (:type data) value)
+                                                     "selected"
+                                                     nil)}
+                                    text)))))))
 
